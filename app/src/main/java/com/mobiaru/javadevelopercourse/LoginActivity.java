7123751f6efacbe503dbbe26a5dd3d5e4f24e760
azhareus <azhar.hj@gmail.com>
2021-11-10 19:25:33 -0500

    created signup page

--- app/src/main/java/com/mobiaru/javadevelopercourse/LoginActivity.java
+++ app/src/main/java/com/mobiaru/javadevelopercourse/LoginActivity.java
@@ -2,13 +2,44 @@ package com.mobiaru.javadevelopercourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btLogin);
+        tvSignup = findViewById(R.id.tvSignup);
+
+        SpannableString content = new SpannableString("Don't have an account? Sign Up!");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tvSignup.setText(content);
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/mobiaru/javadevelopercourse/SignupActivity.java
+++ app/src/main/java/com/mobiaru/javadevelopercourse/SignupActivity.java
@@ -1,14 +1,46 @@
 package com.mobiaru.javadevelopercourse;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private TextView tvBackToLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignup = findViewById(R.id.btSignUp);
+        tvBackToLogin = findViewById(R.id.tvBackToLogin);
+
+        SpannableString content = new SpannableString("Return to Login");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tvBackToLogin.setText(content);
+
+        tvBackToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onclick back to login button");
+                Intent i = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
